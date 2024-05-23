a97c5943ae8124838a643bf96d651af5ae838d24
felipefl200 <felipefl200@gmail.com>
2024-05-23 15:05:55 -0300

    fix: add quantity to total price

--- src/helpers/price.ts
+++ src/helpers/price.ts
@@ -16,15 +16,17 @@ export const formatCurrency = (value: number): string => {
     }).format(value)
 }
 
-export const computedProductTotalPrice = (product: Product): string => {
+export const computedProductTotalPrice = (product: Product, quantity: number = 1): string => {
     if (product.discountPercentage === 0) {
+        let total = Number(product.price) * quantity;
         return Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL"
-        }).format(Number(product.price))
+        }).format(Number(total))
     }
+
     let discount = Number(product.price) * (product.discountPercentage / 100)
-    let totalWithDiscount = Number(product.price) - Number(discount)
+    let totalWithDiscount = (Number(product.price) - Number(discount)) * quantity
     return Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
